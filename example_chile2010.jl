ebe6e86d34c7d97783f4d11dc497ebf3b2312925
Takuya Miyashita <miyashita@hydrocoast.jp>
2018-10-03 04:13:56 +0900

    remove flags

--- example_chile2010.jl
+++ example_chile2010.jl
@@ -8,18 +8,6 @@ end
     include("src/Claw.jl")
 #end
 
-#= *** load, plot and plot if true **** =#
-#= booltopo[load,plot,print]  =# booltopo=
-[1,1,0] .|> Bool
-#= booldtopo[load,plot,print]  =# booldtopo=
-[0,0,0] .|> Bool
-#= boolgauge[load,plot,print] =# boolgauge=
-[0,0,0] .|> Bool
-#= booleta[load,plot,print] =# booleta=
-[0,0,0] .|> Bool
-# animation
-anim = 1 |> Bool
-
 ## file paths
 fdir = joinpath(CLAW,"geoclaw/examples/tsunami/chile2010/_output")
 outdir = "./fig/chile2010"
@@ -29,49 +17,36 @@ using Printf: @printf, @sprintf
 using Plots; pyplot()
 
 # Topography
-if booltopo[1]
     # load
     topofile, _ = Claw.topodata(fdir)
     geo = Claw.LoadTopo(topofile);
-    if booltopo[2]
-        # conditions
-        Plots.clibrary(:cmocean)
-        # plot
-        plt = Claw.PlotTopo(geo, clim=(-6000,6000))
-        plt = Plots.plot!(plt, xlabel="Longitude", ylabel="Latitude", guidefont=font(12))
-        plt = Plots.plot!(plt, tickfont=font(10))
-        Plots.plot!(plt,show=true)
-        if booltopo[3]
-            # save figure(s)
-            Plots.savefig(plt, joinpath(outdir,"topo.svg"))
-        end
-    end
+    # conditions
+    Plots.clibrary(:cmocean)
+    # plot
+    plt = Claw.PlotTopo(geo, clim=(-6000,6000))
+    plt = Plots.plot!(plt, xlabel="Longitude", ylabel="Latitude", guidefont=font(12))
+    plt = Plots.plot!(plt, tickfont=font(10))
+    Plots.plot!(plt,show=true)
+    # save figure(s)
+    Plots.savefig(plt, joinpath(outdir,"topo.svg"))
     # coastal lines
     #plt = Claw.CoastalLines(geo)
-end
 
 # Deformation
-if booldtopo[1]
     # load
     dtopofile, _ = Claw.dtopodata(fdir)
     deform = Claw.LoadDeform(dtopofile);
-    if booldtopo[2]
-        # conditions
-        Plots.clibrary(:colorcet)
-        # plot
-        plt = Claw.PlotDeform(deform, clim=(-2.0,2.0))
-        plt = Plots.plot!(plt, xlabel="Longitude", ylabel="Latitude", guidefont=font(12))
-        plt = Plots.plot!(plt, tickfont=font(10))
-        Plots.plot!(plt,show=true)
-        if booldtopo[3]
-            # save figure(s)
-            Plots.savefig(plt, joinpath(outdir,"dtopo.svg"))
-        end
-    end
-end
+    # conditions
+    Plots.clibrary(:colorcet)
+    # plot
+    plt = Claw.PlotDeform(deform, clim=(-2.0,2.0))
+    plt = Plots.plot!(plt, xlabel="Longitude", ylabel="Latitude", guidefont=font(12))
+    plt = Plots.plot!(plt, tickfont=font(10))
+    Plots.plot!(plt,show=true)
+    # save figure(s)
+    Plots.savefig(plt, joinpath(outdir,"dtopo.svg"))
 
 # Gauge
-if boolgauge[1]
     # load Sim.
     params = Claw.GeoData(fdir)
     gauges = Claw.LoadGauge(fdir, eta0=params.eta0)
@@ -83,46 +58,39 @@ if boolgauge[1]
     # Constructor, gauge(label,id,nt,loc,time,eta)
     gaugeobs=Claw.gauge("Gauge 32412 Obs.",32412,size(obs,1),gauges[1].loc,obs[:,1],obs[:,2])
 
-    if boolgauge[2]
-        tickv=0:3600:3600*9
-        tickl=[@sprintf("%d",i) for i=0:9]
-        xl="Time since earthquake (hour)"
-        yl="Amplitude (m)"
-        # plot Sim.
-        plt = Claw.PlotWaveforms(gauges)
-        # plot Obs..
-        plt = Claw.PlotWaveform!(plt, gaugeobs, lc=:black, lw=.5, ls=:dash)
-        plt = plot!(plt,xlims=(-1000.,33000), ylim=(-0.15,0.25))
-        plt = plot!(plt,xlabel=xl, ylabel=yl, guidefont=font(12), legendfont=font(10))
-        plt = plot!(tickfont=font(10), xticks=(tickv,tickl))
-        Plots.plot!(plt,show=true)
-        if boolgauge[3]
-            # save figure(s)
-            Plots.savefig(plt, joinpath(outdir,"gauge32412.svg"))
-        end
-    end
-end
+    tickv=0:3600:3600*9
+    tickl=[@sprintf("%d",i) for i=0:9]
+    xl="Time since earthquake (hour)"
+    yl="Amplitude (m)"
+    # plot Sim.
+    plt = Claw.PlotWaveforms(gauges)
+    # plot Obs..
+    plt = Claw.PlotWaveform!(plt, gaugeobs, lc=:black, lw=.5, ls=:dash)
+    plt = plot!(plt,xlims=(-1000.,33000), ylim=(-0.15,0.25))
+    plt = plot!(plt,xlabel=xl, ylabel=yl, guidefont=font(12), legendfont=font(10))
+    plt = plot!(tickfont=font(10), xticks=(tickv,tickl))
+    Plots.plot!(plt,show=true)
+    # save figure(s)
+    Plots.savefig(plt, joinpath(outdir,"gauge32412.svg"))
 
 # Free water surface
-if booleta[1]
     # load
     amrall = Claw.LoadSurface(fdir)
-    if booleta[2]
         # conditions
         cl=(-0.5, 0.5)
         Plots.clibrary(:colorcet)
         # plot
         plt = Claw.PlotTimeSeries(amrall, clim=cl)
         # show gauge locations if any
-        if boolgauge[1] && !isempty(gauges)
-            plt = map(i->Claw.PlotGaugeLocs!(i,gauges,ms=4,mfc=:black),plt)
-        end
-        if booleta[3]
-            # save figrue(s)
-            Claw.PrintPlots(plt, outdir)
-        end
-    end
-end
+        #if !isempty(gauges)
+        #    plt = map(i->Claw.PlotGaugeLocs!(i,gauges,ms=4,mfc=:black),plt)
+        #end
+        # save figrue(s)
+        Claw.PrintPlots(plt, outdir)
+
+#=
+# make animation
 if anim && Sys.islinux()
     run(`./animation.sh $outdir step eta`)
 end
+=#
--- example_ike.jl
+++ example_ike.jl
@@ -4,19 +4,7 @@
 if !(@isdefined CLAW)
     include("./CLAWPATH.jl")
 end
-#if !(@isdefined Claw)
-    include("src/Claw.jl")
-#end
-
-#= *** load, plot and plot if true **** =#
-#= booltopo[load,plot,print]  =# booltopo=
-[0,0,0] .|> Bool
-#= boolgauge[load,plot,print] =# boolgauge=
-[1,1,1] .|> Bool
-#= booleta[load,plot,print]   =# booleta=
-[0,0,0] .|> Bool
-#= boolstorm[load,plot,print] =# boolstorm=
-[0,0,0] .|> Bool
+include("./src/Claw.jl")
 
 ## ike
 fdir = joinpath(CLAW,"geoclaw/examples/storm-surge/ike/_output")
@@ -25,84 +13,59 @@ outdir = "./fig/ike"
 using Printf: @printf, @sprintf
 using Plots; pyplot()
 
+
 # Topography
-if booltopo[1]
     # load
     topofile, _ = Claw.topodata(fdir)
     geo = Claw.LoadTopo(topofile);
-    if booltopo[2]
-        # conditions
-        Plots.clibrary(:cmocean)
-        # plot
-        plt = Claw.PlotTopo(geo, clim=(-4000,4000))
-        Plots.plot!(plt,show=true)
-        if booltopo[3]
-            # save figure(s)
-            Plots.savefig(plt, joinpath(outdir,"topo.svg"))
-        end
-    end
-end
+    Plots.clibrary(:cmocean)
+    # plot
+    plt = Claw.PlotTopo(geo, clim=(-4000,4000))
+    Plots.plot!(plt,show=true)
+    # save
+    Plots.savefig(plt, joinpath(outdir,"topo.svg"))
 
 # Gauge
-if boolgauge[1]
     # load
     params = Claw.GeoData(fdir)
     gauges = Claw.LoadGauge(fdir, eta0=params.eta0)
-    if boolgauge[2]
-        tickv=-3*24*3600:12*3600:1*24*3600
-        tickl=[@sprintf("%d",i) for i=-3*24:12:1*24]
-        xl="Hours relative to landfall"
-        yl="Surface (m)"
-        # plot
-        plt = Claw.PlotWaveforms(gauges)
-        plt = plot!(plt,xlabel=xl, ylabel=yl, guidefont=font(12),
-                    legend=:topleft, legendfont=font(10), grid=true)
-        plt = plot!(tickfont=font(10), xticks=(tickv,tickl))
-        Plots.plot!(plt,show=true)
-        if boolgauge[3]
-            # save figure(s)
-            Plots.savefig(plt, joinpath(outdir,"allgauges.svg"))
-        end
-    end
+    # plot
+    tickv=-3*24*3600:12*3600:1*24*3600
+    tickl=[@sprintf("%d",i) for i=-3*24:12:1*24]
+    xl="Hours relative to landfall"
+    yl="Surface (m)"
+    plt = Claw.PlotWaveforms(gauges)
+    plt = plot!(plt,xlabel=xl, ylabel=yl, guidefont=font(12),
+                legend=:topleft, legendfont=font(10), grid=true)
+    plt = plot!(tickfont=font(10), xticks=(tickv,tickl))
+    Plots.plot!(plt,show=true)
+
+    # save figure(s)
+    Plots.savefig(plt, joinpath(outdir,"allgauges.svg"))
     # plot gauges for each window
     # plts = Claw.PlotWaveform.(gauges)
     # plts = Claw.PrintPlots(plts,outdir,prefix="gauge",startnumber=1)
-end
 
 # Free water surface
-if booleta[1]
     # load
     amrall = Claw.LoadSurface(fdir)
-    if booleta[2]
-        # conditions
-        cl=(0.0, 2.0)
-        cpt=:rainbow
-        # plot
-        plt = Claw.PlotTimeSeries(amrall, bound=true, gridnumber=true, clim=cl, cmap=cpt)
-        # show gauge locations if any
-        if boolgauge[1] && !isempty(gauges)
-            plt = map(i->Claw.PlotGaugeLocs!(i,gauges,ms=4),plt)
-        end
-        if booleta[3]
-            # save figrue(s)
-            Claw.PrintPlots(plt, outdir)
-        end
-    end
-end
+    # conditions
+    cl=(0.0, 2.0)
+    cpt=:rainbow
+    # plot
+    plt = Claw.PlotTimeSeries(amrall, bound=true, gridnumber=true, clim=cl, cmap=cpt)
+    # show gauge locations if any
+    #plt = map(i->Claw.PlotGaugeLocs!(i,gauges,ms=4),plt)
+    # save figrue(s)
+    Claw.PrintPlots(plt, outdir)
 
 # Wind and SLP
-if boolstorm[1]
     # load
     storm = Claw.LoadStorm(fdir)
-    if boolstorm[2]
-        # conditions
-        Plots.clibrary(:misc)
-        # plot
-        plt = Claw.PlotTimeSeries(storm, cmap=:heat_r)
-        plt = Claw.PlotWindField!(plt, storm, 5, len=0.05)
-        if boolstorm[3]
-            # save figure(s)
-            Claw.PrintPlots(plt, outdir, prefix="slp")
-        end
-    end
-end
+    # conditions
+    Plots.clibrary(:misc)
+    # plot
+    plt = Claw.PlotTimeSeries(storm, cmap=:heat_r)
+    plt = Claw.PlotWindField!(plt, storm, 5, len=0.05)
+    # save figure(s)
+    Claw.PrintPlots(plt, outdir, prefix="slp")
