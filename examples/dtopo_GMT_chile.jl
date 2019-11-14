ffe512f018501d60508a689cc4f99842d329f103
Takuya Miyashita <miyashita@hydrocoast.jp>
2019-11-15 02:11:39 +0900

    fix dtopo GMT

--- examples/dtopo_GMT_chile.jl
+++ examples/dtopo_GMT_chile.jl
@@ -14,7 +14,7 @@ dtopo = Claw.LoadDeform(dtopofile)
 # makegrd
 G = Claw.geogrd(dtopo; V=true)
 # makecpt
-cpt = GMT.makecpt(; C=:polar, T="-1.0/1.0", D=true)
+cpt = GMT.makecpt(; C=:polar, T="-3.0/3.0", D=true)
 
 # plot
 region = Claw.getR(dtopo)
@@ -22,7 +22,8 @@ proj = Claw.getJ("X10d", Claw.axesratio(dtopo))
 
 GMT.grdimage(G, C=cpt, J=proj, R=region, B="a5f5 neSW", Q=true, V=true)
 GMT.colorbar!(J=proj, R=region, B="xa0.5f0.5 y+l\"(m)\"", D="jBR+w10.0/0.3+o-1.5/0.0", V=true)
-GMT.coast!(J=proj, R=region, D=:i, W=:thinnest, V=true)
+GMT.coast!(J=proj, R=region, D=:i, W="thinnest,gray20", V=true)
+GMT.grdcontour!(G, J=proj, R=region, C=1, A=2, W=:black)
 
 # save
 cp(GMT.fname_out(Dict())[1], "fig/chile2010_dtopo.ps", force=true)
