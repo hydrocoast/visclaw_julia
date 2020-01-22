b716ac3d7774c2551a088e720a5a483c0c6b221e
Takuya Miyashita <miyashita@hydrocoast.jp>
2020-01-22 18:28:46 +0900

    specification of regions: GridNumber!, DrawBound

--- src/Plots2D.jl
+++ src/Plots2D.jl
@@ -58,6 +58,8 @@ function PlotsAMR2D!(plt, tiles::AbstractVector{VisClaw.AMRGrid}; wind::Bool=fal
 
     ## the number of tiles
     ntile = length(tiles)
+    ##
+    nplot_org = plt.n
 
     ## display each tile
     for i = 1:ntile
@@ -66,8 +68,18 @@ function PlotsAMR2D!(plt, tiles::AbstractVector{VisClaw.AMRGrid}; wind::Bool=fal
         y = [tiles[i].ylow, tiles[i].ylow+tiles[i].dy*tiles[i].my]
 
         ## grid info
-        xvec = collect(Float64, x[1]-0.5tiles[i].dx:tiles[i].dx:x[2]+0.5tiles[i].dx+1e-4);
-        yvec = collect(Float64, y[1]-0.5tiles[i].dy:tiles[i].dy:y[2]+0.5tiles[i].dy+1e-4);
+        xvec = collect(Float64, x[1]-0.5tiles[i].dx:tiles[i].dx:x[2]+0.5tiles[i].dx+1e-4)
+        yvec = collect(Float64, y[1]-0.5tiles[i].dy:tiles[i].dy:y[2]+0.5tiles[i].dy+1e-4)
+
+        ## check whether the tile is on the domain
+        if xlims!=nothing
+            if (xvec[end] < xlims[1]) | (xlims[2] < xvec[1]); continue; end
+        end
+        if ylims!=nothing
+            if (yvec[end] < ylims[1]) | (ylims[2] < yvec[1]); continue; end
+        end
+
+
         ## adjust data
         val = zeros(tiles[i].my+2,tiles[i].mx+2)
         if !wind
@@ -85,6 +97,13 @@ function PlotsAMR2D!(plt, tiles::AbstractVector{VisClaw.AMRGrid}; wind::Bool=fal
 
     end
 
+    # check the number of added series
+    if plt.n == nplot_org
+        println("Nothing to be plotted. Check xlims and ylims you specified.")
+        return plt
+    end
+
+
     ## xlims, ylims
     x1, x2, y1, y2 = VisClaw.getlims(tiles)
     xrange = (x1, x2)
@@ -99,8 +118,10 @@ function PlotsAMR2D!(plt, tiles::AbstractVector{VisClaw.AMRGrid}; wind::Bool=fal
     #if !isempty(clim); plt = Plots.plot!(plt, clims=clim); end
 
     ## colorbar
-    for i = 2:ntile; plt.series_list[i].plotattributes[:colorbar_entry] = false; end
-    plt.series_list[1].plotattributes[:colorbar_entry] = true
+    for i = nplot_org+1:plt.n; plt.series_list[i].plotattributes[:colorbar_entry] = false; end
+    plt.series_list[nplot_org+1].plotattributes[:colorbar_entry] = true
+    #for i = 2:ntile; plt.series_list[i].plotattributes[:colorbar_entry] = false; end
+    #plt.series_list[1].plotattributes[:colorbar_entry] = true
 
     ## Appearance
     plt = Plots.plot!(plt, axis_ratio=:equal, grid=false, bginside=bginside, colorbar=true, colorbar_title=cbtitle)
@@ -119,7 +140,10 @@ PlotsAMR2D!(Plots.plot(), tiles; kwargs...)
 """
 Function: add the grid numbers
 """
-function GridNumber!(plt, tiles; font::Plots.Font=Plots.font(12, :hcenter, :black))
+function GridNumber!(plt, tiles; font::Plots.Font=Plots.font(12, :hcenter, :black),
+                     xlims::Tuple=(),
+                     ylims::Tuple=())
+
     ## the number of tiles
     ntile = length(tiles)
     for i = 1:ntile
@@ -127,6 +151,15 @@ function GridNumber!(plt, tiles; font::Plots.Font=Plots.font(12, :hcenter, :blac
         x = [tiles[i].xlow, tiles[i].xlow+tiles[i].dx*tiles[i].mx]
         y = [tiles[i].ylow, tiles[i].ylow+tiles[i].dy*tiles[i].my]
         ann = @sprintf("%02d", tiles[i].gridnumber)
+
+        ## check whether the tile is on the domain
+        if !isempty(xlims)
+            if (mean(x) < xlims[1]) | (xlims[2] < mean(x)); continue; end
+        end
+        if !isempty(ylims)
+            if (mean(y) < ylims[1]) | (ylims[2] < mean(y)); continue; end
+        end
+
         ## annotations
         plt = Plots.plot!(plt, annotations=(mean(x),mean(y), Plots.text(ann, font)))
     end
@@ -143,13 +176,19 @@ function DrawBound!(plt, tiles; kwargs...)
     # parse keyword args
     kwdict = KWARG(kwargs)
 
+    # -----------------------------
     # linestyle
     linestyle, kwdict = VisClaw.parse_linestyle(kwdict)
     if linestyle == nothing; linestyle=:solid; end
+    # -----------------------------
     # linecolor
     linecolor, kwdict = VisClaw.parse_linecolor(kwdict)
     if linecolor == nothing; linecolor=:black; end
-
+    # -----------------------------
+    # xlims, ylims
+    xlims, kwdict = VisClaw.parse_xlims(kwdict)
+    ylims, kwdict = VisClaw.parse_ylims(kwdict)
+    # -----------------------------
 
     ## the number of tiles
     ntile = length(tiles)
@@ -157,6 +196,15 @@ function DrawBound!(plt, tiles; kwargs...)
         ## set the boundary
         x = [tiles[i].xlow, tiles[i].xlow+tiles[i].dx*tiles[i].mx]
         y = [tiles[i].ylow, tiles[i].ylow+tiles[i].dy*tiles[i].my]
+
+        ## check whether the tile is on the domain
+        if xlims!=nothing
+            if (x[2] < xlims[1]) | (xlims[2] < x[1]); continue; end
+        end
+        if ylims!=nothing
+            if (y[2] < ylims[1]) | (ylims[2] < y[1]); continue; end
+        end
+
         plt = Plots.plot!(plt,
               [x[1], x[1], x[2], x[2], x[1]],
               [y[1], y[2], y[2], y[1], y[1]],
